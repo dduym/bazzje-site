6e40d0bbda7adaa0614013935ba212c630e3e8db
Dimitri <dimitri.duym@student.hogent.be>
2026-01-11 13:35:40 +0100

    Under construction page

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/index.css'; // let op dat dit pad correct is
+
+const App: React.FC = () => {
+  return (
+    <div className='app-container'>
+      <div className='center-card'>Under construction</div>
+    </div>
+  );
+};
+
+export default App;
